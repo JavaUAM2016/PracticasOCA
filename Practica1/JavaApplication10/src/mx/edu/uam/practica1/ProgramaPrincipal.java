ce3333fb75c8e6b3e579a9d2c9f3bff0e6e11939
sidlors <sidlors@gmail.com>
2016-05-14 14:59:44 -0500

    Crea Practica 1 en uami
    Es nuestra pimer practica de java, el calsico hola mundo

---
+++ Practica1/JavaApplication10/src/mx/edu/uam/practica1/ProgramaPrincipal.java
@@ -0,0 +1,16 @@
+package mx.edu.uam.practica1;
+
+/**
+ *
+ * @author juanmanuel
+ */
+public class ProgramaPrincipal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo En UAM-I");
+    }
+    
+}
