380c62a8a8026dea4f2650e6ea38334f28fccad3
Invited <invited@LDCBIs-Mac-mini-16.local>
2016-05-28 14:30:02 -0500

    Mi practica ferston

---
+++ Practica3/ferston/javaApp/src/mx/edu/uam/practica3/aparatos/Ventilador.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.uam.practica3.aparatos;
+
+/**
+ *
+ * @author invited
+ */
+public class Ventilador {
+    
+    private String marca;
+     private boolean encendido;
+     
+    
+}
