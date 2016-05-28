7c564b0d5e128d583b1798d4f4bb72e0efcb6d89
Juan Perez Jolote <sidlord@hotmail.com>
2016-05-28 14:38:16 -0500

    Merge

---
+++ Practica3/JavaApp/src/mx/edu/uam/practica3/AppMain.java
@@ -0,0 +1,28 @@
+package mx.edu.uam.practica3;
+
+import mx.edu.uam.practica3.aparatos.BlueRay;
+import mx.edu.uam.practica3.controles.ControlRemoto;
+import mx.edu.uam.practica3.aparatos.Television;
+import mx.edu.uam.practica3.aparatos.Usuario;
+
+/**
+ *
+ * @author jhernandezn
+ */
+public class AppMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        BlueRay sony;
+        BlueRay panasonic;
+        Television RCA,Toshiba,LG;
+        Toshiba=new Television();
+        RCA=new Television();
+        LG=new Television();        
+        
+    }
+    
+}
