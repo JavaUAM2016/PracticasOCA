52f1e6ec4827c2d8eefd48c5b28d7691edacfaf9
Invited <invited@LDCBIs-Mac-mini-7.local>
2016-05-28 13:21:57 -0500

    Correccion de error

--- Practica1/JavaApp/src/mx/edu/uam/practica1/ProgramaPrincipal.java
+++ Practica1/JavaApp/src/mx/edu/uam/practica1/ProgramaPrincipal.java
@@ -10,11 +10,9 @@ public class ProgramaPrincipal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("Hola Mundo En UAM-I, java OCA...");
-=======
-        System.out.println("Hola Mundo En UAM-I, Java OCA");
->>>>>>> 7fb75e804a7912d319d42762c954ad8a7d2cd54d
+
     }
     
 }
