88f5c0a24a3b988d4d6a60e11c34f1aef2c2920b
Invited <invited@LDCBIs-Mac-mini-11.local>
2016-05-28 14:30:02 -0500

    Mi practica monilezama

---
+++ Practica3/monilezama/JavaApp/src/mx/edu/uam/practica3/controles/ControlVentilador.java
@@ -0,0 +1,55 @@
+
+package mx.edu.uam.practica3.controles;
+
+
+import mx.edu.uam.practica3.aparatos.Ventilador;
+
+public class ControlVentilador {
+    private int velMax=3;
+    
+    public void prende(Ventilador ven){
+        if(ven.isEncendido()==false){
+            ven.setEncendido(true);
+           System.out.println("Se prendio el ventilador...");
+        }else
+             ven.setEncendido(false);
+            System.out.println("Se apago el ventilador...");
+    }
+    
+    
+    
+    public void subeVel(Ventilador ven){
+       if(ven.isEncendido()){
+           if(ven.getVelActual()<velMax){
+               ven.setVelActual(ven.getVelActual()+1);
+               System.out.println("Se subio la velocidad..") ;
+           }
+            
+       }else
+            System.out.println("El ventilador esta apagado, no se puede subir la velocidad..");
+    }
+    public void bajaVel(Ventilador ven){
+        if(ven.isEncendido()){
+            if(ven.getVelActual()>0){
+                ven.setVelActual(ven.getVelActual()-1);
+                System.out.println("Se bajo la velocidad..");
+            }
+        }else
+            System.out.println("El ventilador  esta apagado no se puede bajar la velocidad..");
+        
+    }
+    
+    public void girar(Ventilador ven){
+        if(ven.isEncendido()){
+            if(ven.isGirando()){
+                System.out.println("El ventilador dejo de girar..");
+            }else{
+                System.out.println("El ventilador esta girando...");
+            }
+            
+        }else{
+            System.out.println("El ventilador esta apagado no puede girar..");
+        }
+    }
+}
+
