1b594db368ebf01d950c42cc0ed71b3d0f9030b0
invited <invited@192.168.0.44>
2016-06-11 14:27:04 -0500

    Practica 4 AlfonsoLomas se agrega la prueba unitaria de cambio de canal

--- Practica4/AlfonsoLomas/JavaApp4/test/mx/edu/uam/practica4/test/AppMainTest.java
+++ Practica4/AlfonsoLomas/JavaApp4/test/mx/edu/uam/practica4/test/AppMainTest.java
@@ -1,5 +1,11 @@
 package mx.edu.uam.practica4.test;
 
+import mx.edu.uam.practica4.control.ControlRemoto;
+import mx.edu.uam.practica4.control.ControlTv;
+import mx.edu.uam.practica4.electrodomestico.Electrodomestico;
+import mx.edu.uam.practica4.electrodomestico.Television;
+import mx.edu.uam.practica4.familia.Familiar;
+import mx.edu.uam.practica4.familia.Padre;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -12,37 +18,48 @@ import static org.junit.Assert.*;
  * @author invited
  */
 public class AppMainTest {
-    
+
+    private Television tv;
+    private ControlRemoto controlTV;
+    private Electrodomestico blueRay;
+    private ControlRemoto controlBlue;
+    private ControlRemoto[] controles;
+    private Familiar papa;
+
     public AppMainTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
-        System.out.println("Before class");
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
-        System.out.println("After class");
     }
-    
+
     @Before
     public void setUp() {
-        System.out.println("before");
-    }
-    
-    @After
-    public void tearDown() {
-        System.out.println("after");
+        tv = new Television();
+        controlTV = new ControlTv();
+        //blueRay = new BlueRay();
+        //controlBlue = new ControlBlueRay();
+        papa = new Padre();
+        controles=new ControlRemoto[2];
+        controles[0]=controlTV;
+        controles[1]= controlBlue ;
+
+
+
+        papa.setControlRemoto(controles);
+        controlTV = papa.getControlRemoto()[0];
+        controlTV.encender(tv);
+        ((ControlTv) controlTV).cambiaCanal(tv, 25);
     }
 
     @Test
-    public void hello() {
-        System.out.println("Prueba unitaria :  test hello");
+    public void testApp() {
+
+        assertSame(25, tv.getCanalActual());
     }
-    
-    @Test
-    public void hello2() {
-        System.out.println("Prueba unitaria 2:  test hello2");
-    }
+
 }
