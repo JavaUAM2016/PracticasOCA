db7bd75f20b0ab297fedfefc3d656303633ce9a8
fernando209311662 <edwin.pingus@gmail.com>
2016-06-11 14:32:14 -0500

    Se corrige logica en clases control de PlayStation3 y Radio

--- Practica4/fernando209311662/JavaPrac4/src/mx/edu/uam/practica4/control/ControlPs3.java
+++ Practica4/fernando209311662/JavaPrac4/src/mx/edu/uam/practica4/control/ControlPs3.java
@@ -4,43 +4,52 @@ import mx.edu.uam.practica4.electrodomestico.PlayStation3;
 
 public class ControlPs3 extends Control{
     
-    public boolean conectaInternet(PlayStation3 ps3){
-        if(!ps3.isWifi()){//wifi apagado
-            System.out.println("PlayStation Network Sin Conexion...\n");
-            return false;
+    public void conectaInternet(PlayStation3 ps3){
+        if(ps3.isEncendido()){
+             if(ps3.isWifi()){
+                System.out.println("Desconectando...");
+                ps3.setWifi(false);
+            }
+            else{
+                System.out.println("Conectando a internet... :)");
+                ps3.setWifi(true);
+            }
         }
-        else{
-            System.out.println("PlayStation Network Conectando...\n");
-            return true;
-        }
+        
     }
     
     public void descargaJuego(PlayStation3 ps3,int tamJuego){
-        if(conectaInternet(ps3))
-            if(tamJuego <= ps3.getCapacidadDiscoDuroMax())
-                System.out.println("Iniciando Descarga...please wait\n");
+        if(ps3.isEncendido()){
+            if(ps3.isWifi())
+                if(tamJuego <= ps3.getCapacidadDiscoDuroMax())
+                    System.out.println("Iniciando Descarga...please wait\n");
+                else
+                    System.out.println("Capacidad Disco Duro Insuficiente..\n");
             else
-                System.out.println("Capacidad Disco Duro Insuficiente..\n");
-        else
-            System.out.println("Comprube su conexion a Internet ??\n");
+                System.out.println("Comprube su conexion a Internet ??\n");
+
+        }
     }
     
     public void cargaJuego(PlayStation3 ps3,int noJuego){
-        if(noJuego > 0 && noJuego <= ps3.getVideoJuegosInstall()){
+        if(ps3.isEncendido()){
+            if(noJuego > 0 && noJuego <= ps3.getVideoJuegosInstall()){
             ps3.setVideoJuegoActual(noJuego);
             System.out.println("Juego ["+ps3.getVideoJuegoActual()+"] Cargando...\n");
         }
         else
             System.out.println("Número de Juego No valido...Exit\n");
+        }  
     }
     
     public void agregaJugador(PlayStation3 ps3){
-        if(ps3.getJugadores() >= 0 && ps3.getJugadores()< ps3.getNoJugadoresMax()){
+        if(ps3.isEncendido()){
+            if(ps3.getJugadores() >= 0 && ps3.getJugadores()< ps3.getNoJugadoresMax()){
             ps3.setJugadores(ps3.getJugadores()+1);
             System.out.println("Nuevo Jugador Agregado..\n");
         }
         else
             System.out.println("Operacion Invalida No se pueden agregar mas Jugadores.\n");
+        }
     }
-    
 }
